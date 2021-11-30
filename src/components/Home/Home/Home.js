6ea25a92cf9fa5170605d604f019ec6fb83b21f4
Fahid Hossain <fahidhossain2021@gmail.com>
2021-11-30 23:10:14 +0600

    skill section done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -3,12 +3,14 @@ import Navigation from '../../Navigation/Navigation';
 import About from '../About.js/About';
 import Banner from '../Banner/Banner';
 import Contact from '../Contact/Contact';
+import Skills from '../Skills/Skills';
 
 const Home = () => {
     return (
         <div className="bg-dark">
             <Navigation></Navigation>
             <Banner></Banner>
+            <Skills></Skills>
             <About></About>
             <Contact></Contact>
         </div>
