3b4cc51c25ffd96644b93ae1ee0d6f9bb32e4f47
Fahid Hossain <fahidhossain2021@gmail.com>
2021-12-02 19:06:39 +0600

    about section modify

--- src/components/Home/About.js/About.js
+++ src/components/Home/About.js/About.js
@@ -5,7 +5,7 @@ import img from "../../../../src/images/dream-bg.png"
 
 const About = () => {
     return (
-        <div id="about" className="text-white bg-dark about-section">
+        <div id="about" className="text-white bg-dark container">
             <img src={img} alt="" className="img-fluid" style={{ width: "4.5rem", float: "right" }} />
             <div className="row">
                 <div data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" className="col-12 col-md-6">
